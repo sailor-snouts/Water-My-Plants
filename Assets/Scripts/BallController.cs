50ef4616887e072fff473a239155d2dfa6cbb3a0
Robert Parker <rparker@kodama.design>
2018-06-23 15:47:42 -0700

    bouncy ball

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour {
+    public float ballInitialVelocity = 600f;
+    private Rigidbody2D rb;
+    private bool ballInPlay;
+
+    void Awake () {
+        this.rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+	
+	void Update () {
+        if (Input.GetButtonDown("Fire1") && ballInPlay == false)
+        {
+            transform.parent = null;
+            ballInPlay = true;
+            rb.isKinematic = false;
+            rb.AddForce(new Vector3(0, ballInitialVelocity, 0));
+        }
+    }
+}
