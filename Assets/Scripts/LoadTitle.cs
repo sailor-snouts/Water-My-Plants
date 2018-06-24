fc6467750be3e2d659dd1e5c81ab513c22cb9195
Joshua Skinner <jkyuusai@gmail.com>
2018-06-23 20:50:42 -0700

    Add splash, title, and credits scenes
    Adds placeholder splash screen with music
    Adds placeholder credits screen that exits back to title screen on keypress
    Adds placeholder title screen with start, credits, and quit buttons
    - Start loads the game scene
    - Credits loads a placeholder credits scene
    - Quit exits the app or the editor player if in Unity

---
+++ Assets/Scripts/LoadTitle.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadTitle : MonoBehaviour {
+
+	void Start () {
+        Invoke("LoadTitleScreen", 3f);
+	}
+	
+	void LoadTitleScreen() {
+        SceneManager.LoadScene(1);
+    }
+}
---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    void LoadGame() {
+        SceneManager.LoadScene(2);
+    }
+}
