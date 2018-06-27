fd22d9d3e7397caa003bdd0ea154b192c9528ee8
Joshua Skinner <jkyuusai@gmail.com>
2018-06-26 18:38:56 -0700

    Add sceneTransitionCanvas prefab
    Show message that tells user how to move between scenes and
    transition between scenes when any key is pressed.
    
    Adds this to every scene except main.

---
+++ Assets/Scripts/SceneTransition.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneTransition : MonoBehaviour {
+
+    [SerializeField] public string transitionMessage = "Press any key to continue";
+    [SerializeField] public string sceneTarget = "Title";
+
+    Text transitionText;
+
+    void Start () {
+        transitionText = GetComponent<Text>();
+        transitionText.text = transitionMessage;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.anyKey) {
+            SceneManager.LoadScene(sceneTarget);
+        }
+    }
+}
