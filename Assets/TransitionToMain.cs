ba5f98781f69ab96c786394c3ca981ef972d341a
Joshua Skinner <jkyuusai@gmail.com>
2018-06-30 16:49:09 -0700

    Use Fader to load main from title
    Skip credits on win and game over screens

---
+++ Assets/TransitionToMain.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class TransitionToMain : MonoBehaviour {
+
+    FadeScenes fadeScenes;
+
+    void Start() {
+       
+        fadeScenes = GetComponent<FadeScenes>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.anyKey) {
+            fadeScenes.LoadSceneAsync("Main");
+        }
+    }
+}
