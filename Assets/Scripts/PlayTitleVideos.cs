6b17eb8fc03d08fd8d12311e6772f93726f9096e
Joshua Skinner <jkyuusai@gmail.com>
2018-06-29 22:18:50 -0700

    Add Title screen animations

---
+++ Assets/Scripts/PlayTitleVideos.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class PlayTitleVideos : MonoBehaviour {
+
+    VideoPlayer[] videoPlayers;    
+
+    // Use this for initialization
+    void Start() {
+        videoPlayers = GetComponents<VideoPlayer>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+       
+        if ((ulong)videoPlayers[0].frame == videoPlayers[0].frameCount) {
+            videoPlayers[1].Play();
+        }
+    }
+}
