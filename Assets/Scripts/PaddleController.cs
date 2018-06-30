729caf719b0a7f3dc1d88e4f5d8c4e05b0993471
Joshua Skinner <jkyuusai@gmail.com>
2018-06-30 13:05:44 -0700

    Add controller support

--- Assets/Scripts/PaddleController.cs
+++ Assets/Scripts/PaddleController.cs
@@ -9,15 +9,34 @@ public class PaddleController : MonoBehaviour {
     private float z;
     private Vector3 pointerPosition;
     private float moveTo;
+    Rigidbody2D rigidBody;
+    [SerializeField]
+    float controllerInputForceMultiplier = 65;
+    private bool useMouse = true;
 
-	void Start () {
+    void Start () {
         this.y = this.transform.position.y;
         this.z = this.transform.position.z;
-	}
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
 	
 	void Update () {
-        this.pointerPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        this.moveTo = Mathf.Clamp(this.pointerPosition.x, this.minX, this.maxX);
-        this.transform.position = new Vector3(moveTo, this.y, this.z);
+        /**
+         * Tweak feel of paddle when using controller by changing the force multiplier here,
+         * and the linear drag on the paddle's rigidBody2D.
+         */
+        float controllerInput = Input.GetAxis("Horizontal");
+        if (controllerInput != 0) {
+            rigidBody.AddForce(new Vector2(controllerInput * controllerInputForceMultiplier, this.y));
+            useMouse = false;
+        } else if(useMouse) {
+            /**
+             * Mouse can't be on when controller is active, otherwise it will constantly reset the position
+             * of the paddle, even when the mouse is offscreen.
+             */
+            this.pointerPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            this.moveTo = Mathf.Clamp(this.pointerPosition.x, this.minX, this.maxX);
+            this.transform.position = new Vector3(moveTo, this.y, this.z);
+        }
     }
 }
