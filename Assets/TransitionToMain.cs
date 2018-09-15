437db592a9dccb9f61b41b79a4b4c4a000276e75
Robert Parker <rparker@kodama.design>
2018-09-14 19:43:30 -0700

    placeholder loading scene

--- Assets/TransitionToMain.cs
+++ Assets/TransitionToMain.cs
@@ -3,20 +3,49 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class TransitionToMain : MonoBehaviour {
 
-    FadeScenes fadeScenes;
-
-    void Start() {
-       
-        fadeScenes = GetComponent<FadeScenes>();
-    }
+    [SerializeField]
+    private string LoadingScene = "Scenes/Loading";
+    [SerializeField]
+    private string Scene = "Scenes/Main";
+    [SerializeField]
+    private float checkDelay = 5f;
+    [SerializeField]
+    private bool isLoaded = false;
+    [SerializeField]
+    private bool isLocked = false;
+    AsyncOperation async;
 
     // Update is called once per frame
     void Update() {
-        if (Input.anyKey) {
-            fadeScenes.LoadSceneAsync("Main");
+        if (!this.isLocked && Input.anyKey)
+        {
+            this.isLocked = true;
+            this.isLoaded = false;
+            SceneManager.LoadScene(this.LoadingScene, LoadSceneMode.Additive);
+            StartCoroutine(LoadNewScene(this.Scene));
         }
+        else if (this.isLocked && this.isLoaded)
+        {
+            this.async.allowSceneActivation = true;
+        }
+    }
+
+    IEnumerator LoadNewScene(string scene)
+    {
+        yield return new WaitForSeconds(this.checkDelay);
+
+        this.async = Application.LoadLevelAsync(scene);
+        async.allowSceneActivation = false;
+
+        while (async.progress < 0.9f)
+        {
+            yield return null;
+        }
+
+        this.isLoaded = true;
     }
 }
