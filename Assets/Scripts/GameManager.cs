afdb6a94fe1795469a8563a932cb34f082a569ce
Robert Parker <rparker@kodama.design>
2018-11-23 12:13:33 -0800

    new HUD

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,51 +7,53 @@ public class GameManager : MonoBehaviour
     public FadeScenes fadeScenes;
     public BallController ball;
     public WaterTank tank;
-    public ScoreBoard scoreBoard;
     public ForestController forest;
     public VoiceController voices;
     public GameObject ground;
+    private ProgressBar progressBar;
     private int groundCount;
     private SpriteRenderer[] spriteR;
     private float percepitationRate = 0.8f;
+    private float completion = 0f;
     
 
 	void Start ()
     {
         this.spriteR = this.ground.GetComponentsInChildren<SpriteRenderer>();
         this.groundCount = this.spriteR.Length;
+        this.progressBar = FindObjectOfType<ProgressBar>();
         QualitySettings.vSyncCount = 0;
         Application.targetFrameRate = 60;
     }
 	
 	void Update ()
-    {        
-        // show level progress
-        float completion = this.forest.GetCompletion(); ;
-        float prettyCompletion = Mathf.Floor(Mathf.Clamp(completion * 105f, 0f, 100f));
-
-        // scoreboard
-        scoreBoard.SetScore(prettyCompletion);
-
-        float delta = 1f / (float) this.groundCount;
-        int i = 0;
-        for (i = 0; i < this.groundCount; i++)
+    {
+        if (this.forest.GetCompletion() != this.completion)
         {
-            if(completion+delta > i*delta)
-            {
-                Color tmp = this.spriteR[i].color;
-                tmp.a = 1f;
-                this.spriteR[i].color = tmp;
-            }
-            else
-            {
-                Color tmp = this.spriteR[i].color;
-                tmp.a = 0f;
-                this.spriteR[i].color = tmp;
-            }
-        }
+            this.completion = this.forest.GetCompletion();
 
-        this.voices.HandleVoices(completion);
+            this.progressBar.SetGoal(completion);
+
+            float delta = 1f / (float)this.groundCount;
+            int i = 0;
+            for (i = 0; i < this.groundCount; i++)
+            {
+                if (completion + delta > i * delta)
+                {
+                    Color tmp = this.spriteR[i].color;
+                    tmp.a = 1f;
+                    this.spriteR[i].color = tmp;
+                }
+                else
+                {
+                    Color tmp = this.spriteR[i].color;
+                    tmp.a = 0f;
+                    this.spriteR[i].color = tmp;
+                }
+            }
+
+            this.voices.HandleVoices(completion);
+        }
 
         // lose
         if (!this.ball.isAlive)
@@ -61,7 +63,7 @@ public class GameManager : MonoBehaviour
         }
 
         // win
-        if(prettyCompletion == 100f)
+        if(completion >= 0.99f)
         {
             this.fadeScenes.LoadScene("Win");
             return;
---
+++ Assets/Scripts/ProgressBar.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProgressBar : MonoBehaviour
+{
+    public Transform mask;
+    public SpriteRenderer flower;
+    [SerializeField]
+    private float fill = 0;
+    private float current = 0;
+    private float yVelocity = 0.0F;
+    private float fillRate = 0.35f;
+    
+    void Update ()
+    {
+        float amountToFillThisFrame = Mathf.SmoothDamp(this.mask.localScale.y, this.fill, ref yVelocity, fillRate);
+        this.mask.localScale = new Vector3(1, amountToFillThisFrame, 1);
+        if (this.fill >= 0.99f)
+        {
+            flower.enabled = true;
+        }
+    }
+
+    public void SetGoal(float completion)
+    {
+        this.current = Mathf.Clamp01(completion);
+        this.fill = Mathf.InverseLerp(0, 1f, this.current);
+    }
+}
